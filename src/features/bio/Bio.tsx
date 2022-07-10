c11eda6d1e2c642443dbc6312161fca44da0bbc4
EduardoCampo <eduardocampo91@gmail.com>
2022-07-09 23:52:21 -0500

    styled components ready and working, proceding to merge

--- src/features/bio/Bio.tsx
+++ src/features/bio/Bio.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { NombresSimpsons, INFO_SIMPSONS } from "./constants";
-import styles from "./styles.module.css";
-import { bioContainer, contenedorBotones, bioImagen, bioNombre, bioDescripcion}  from './styled'
+import { BioContainer, ContenedorBotones, BioImagen, BioNombre, BioDescripcion, BotonBio }  from './styled'
 
 const Bio = () => {
   const [bioActiva, setBioActiva] = useState(
@@ -13,37 +12,34 @@ const Bio = () => {
 
   const crearBotones = () => {
     return Object.keys(INFO_SIMPSONS).map((nombre: string) => (
-      <button
+      <BotonBio
         key={nombre as string}
         onClick={() => onClick(nombre as NombresSimpsons)}
-        className={
-          bioActiva.id === nombre
-            ? styles.botonBioActivo
-            : styles.botonBioInactivo
+        isActive={
+          bioActiva.id === nombre ? true : false
         }
       >
         {nombre}
-      </button>
+      </BotonBio>
     ));
   };
 
   return (
-    <div className={styles.bioContainer}>
-      <div className={styles.contenedorBotones}>{crearBotones()}</div>
+    <BioContainer>
+      <ContenedorBotones>{crearBotones()}</ContenedorBotones>
       <div>
         <div>
-          <img
+          <BioImagen
             src={bioActiva.image}
             alt={bioActiva.nombre}
-            className={styles.bioImagen}
           />
         </div>
         <div>
-          <h3 className={styles.bioNombre}>{bioActiva.nombre}</h3>
-          <p className={styles.bioDescripcion}>{bioActiva.descripcion}</p>
+          <BioNombre>{bioActiva.nombre}</BioNombre>
+          <BioDescripcion>{bioActiva.descripcion}</BioDescripcion>
         </div>
       </div>
-    </div>
+    </BioContainer>
   );
 };
 
--- src/features/bio/styled.ts
+++ src/features/bio/styled.ts
@@ -1,36 +1,56 @@
 import styled from "styled-components";
 
-export const bioContainer = styled.div `
+export default interface iStyles {
+    isActive: boolean;
+}
+
+export const BioContainer = styled.div `
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: flex-start;
     width: 100%;
     height: 100%;
-    `
+    `;
 
-export const contenedorBotones = styled.div `
+export const ContenedorBotones = styled.div `
     display: flex;
     flex-direction: row;
     justify-content: center;
     align-items: center;
     width: 100%;
     margin-bottom: 1rem;
-    `
+    `;
 
-export const bioImagen = styled.img `
+export const BioImagen = styled.img `
     max-width: 200px;
     max-height: 300px;
     margin-bottom: 1rem;
-    `
+    `;
 
-export const bioNombre = styled.h3 `
+export const BioNombre = styled.h3 `
     font-size: 2em;
     margin-bottom: 1rem;
-    `
+    `;
 
-export const bioDescripcion = styled.p `
+export const BioDescripcion = styled.p `
     font-size: 1.3em;
     width: 70%;
     margin: 1rem auto;
-    `
+    `;
+
+export const BotonBio =styled.button<iStyles> `
+    &:hover {
+        cursor: pointer;
+    }
+    border-radius: 5px;
+    border: 1px solid darkgray;
+    box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
+    padding: 1rem;
+    margin: 1rem;
+    font-family: "Homer Simpson Revised", sans-serif;
+    font-size: 1.4rem;
+    background-color: ${( props ) => props.isActive ? "#fdd835" : ""};
+    color: ${( props ) => props.isActive ? "whitesmoke" : ""};
+    text-shadow: ${( props ) => props.isActive ? "2px 2px 0 #000000, 2px -2px 0 #000000, -2px 2px 0 #000000, -2px -2px 0 #000000, 2px 0px 0 #000000, 0px 2px 0 #000000, -2px 0px 0 #000000, 0px -2px 0 #000000;" : ""};
+    `;
